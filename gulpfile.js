98cbb145ce036320e82c71597ccea3da557895e6
psirenny <djtorres0@gmail.com>
2014-07-11 18:51:00 -0400

    remove copy/pasted code from gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,6 @@ var gulp = require('gulp');
 var rimraf = require('gulp-rimraf');
 
 gulp.task('clean', function () {
-  return gulp.src(['node_modules', 'multislot.js', 'test/browser/build'], {read: false})
+  return gulp.src(['node_modules'], {read: false})
     .pipe(rimraf())
 });
